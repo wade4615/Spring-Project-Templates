4ccc108ac564dd314640ff0540abade388a83138
wade4615 <wade4615@gmail.com>
2020-08-11 07:18:52 -0500

    update to spring crud angular app

--- springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplication.java
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot2JpaCrudExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot2JpaCrudExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2JpaCrudExampleApplication.class, args);
+    }
 
 }
